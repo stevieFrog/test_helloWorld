a20ffc38758802dfac9a0278cca1f828616fa40b
stevieFrog <steve@steve-merchant.co.uk>
2018-03-14 08:21:16 +0000

    First Commit

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColour = '#696969';
+
+let defaultStyle = {
+	color: defaultTextColour
+	
+};
+
+
+class Aggregate extends Component{
+	render() {
+		return(
+			<div style={{...defaultStyle, width: "40%", display: 'inline-block'}}>
+				<h2>Number and Text</h2>
+			</div>
+		);
+	}
+}
+
+class Filter extends Component {
+	render() {
+		return(
+			<div style={defaultStyle}>
+				<img/>
+				<input type="text"/>
+				
+			</div>
+		)
+	}
+}
+
+class Playlist extends Component {
+	render() {
+		return(
+			<div style={{...defaultStyle, display: 'inline-block', width: "20%"}}>
+				<img/>
+				<h3> Playlist Name</h3>
+				<ul><li>Song 1</li><li>Song 2</li><li>Song 3</li><li>Song 4</li></ul>
+			</div>
+		);
+		
+	}
+	
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+		<h1>Title</h1>
+		<Aggregate/>
+		<Aggregate/>
+		<Filter/>
+		<Playlist/>
+		<Playlist/>
+		<Playlist/>
+		<Playlist/>
+      </div>
+    );
+  }
+}
+
+export default App;
